acd0151bf87a85606cc8c41bb28c0e372abac727
David Paulino <51489776+uclobby@users.noreply.github.com>
2022-11-01 11:00:40 -0400

    2.0
    Sync with version in UCLobbyTeams module:
    
    https://github.com/uclobby/UCLobbyTeams/blob/main/Source/Public/Get-UcTeamsVersion.ps1

--- Get-TeamsVersion.ps1
+++ Get-TeamsVersion.ps1
@@ -16,6 +16,11 @@
 #
 # Identify 16-bit, 32-bit and 64-bit executables with PowerShell
 # https://gallery.technet.microsoft.com/scriptcenter/Identify-16-bit-32-bit-and-522eae75
+    Param(
+        [string]$Path,
+        [string]$Computer
+    )
+
 
 function Get-UcArch([string]$sFilePath)
 {
@@ -65,53 +70,146 @@ try
     $regexVersion = '("version":")([0-9.]*)'
     $regexRing = '("ring":")(\w*)'
     $regexEnv = '("environment":")(\w*)'
+    $regexCloudEnv = '("cloudEnvironment":")(\w*)'
     $regexRegion = '("region":")([a-zA-Z0-9._-]*)'
-    
+
+    $regexWindowsUser = '("upnWindowUserUpn":")([a-zA-Z0-9@._-]*)'
+    $regexTeamsUserName = '("userName":")([a-zA-Z0-9@._-]*)'
+
     $outTeamsVersion = [System.Collections.ArrayList]::new()
-    
-    $currentDateFormat = [cultureinfo]::CurrentCulture.DateTimeFormat.ShortDatePattern
-    $Profiles = Get-childItem 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | ForEach-Object {Get-ItemProperty $_.pspath } | Where-Object {$_.fullprofile -eq 1}
-    foreach($Profile in $Profiles){
-        $TeamsSettingPath = $Profile.ProfileImagePath + "\AppData\Roaming\Microsoft\Teams\settings.json"
-        if(Test-Path $TeamsSettingPath -ErrorAction SilentlyContinue) {
-            $TeamsSettings = Get-Content -Path $TeamsSettingPath
-            $Version = ""
-            $Ring = ""
-            $Env = ""
-            $Region = ""
-            try {
-                $VersionTemp = [regex]::Match($TeamsSettings,$regexVersion).captures.groups
-                if($VersionTemp.Count -ge 2){
-                    $Version = $VersionTemp[2].value
+
+    if($Path){
+        if (Test-Path $Path -ErrorAction SilentlyContinue) {
+            $TeamsSettingsFiles = Get-ChildItem -Path $Path -Include "settings.json" -Recurse
+            foreach ($TeamsSettingsFile in $TeamsSettingsFiles){
+                $TeamsSettings = Get-Content -Path $TeamsSettingsFile.FullName
+                $Version = ""
+                $Ring = ""
+                $Env = ""
+                $CloudEnv = ""
+                $Region = ""
+                try {
+                    $VersionTemp = [regex]::Match($TeamsSettings, $regexVersion).captures.groups
+                    if ($VersionTemp.Count -ge 2) {
+                        $Version = $VersionTemp[2].value
+                    }
+                    $RingTemp = [regex]::Match($TeamsSettings, $regexRing).captures.groups
+                    if ($RingTemp.Count -ge 2) {
+                        $Ring = $RingTemp[2].value
+                    }
+                    $EnvTemp = [regex]::Match($TeamsSettings, $regexEnv).captures.groups
+                    if ($EnvTemp.Count -ge 2) {
+                        $Env = $EnvTemp[2].value
+                    }
+                    $CloudEnvTemp = [regex]::Match($TeamsSettings, $regexCloudEnv).captures.groups
+                    if ($CloudEnvTemp.Count -ge 2) {
+                        $CloudEnv = $CloudEnvTemp[2].value
+                    }
+                    $RegionTemp = [regex]::Match($TeamsSettings, $regexRegion).captures.groups
+                    if ($RegionTemp.Count -ge 2) {
+                        $Region = $RegionTemp[2].value
+                    }
                 }
-                $RingTemp = [regex]::Match($TeamsSettings,$regexRing).captures.groups
-                if($RingTemp.Count -ge 2){
-                    $Ring = $RingTemp[2].value
+                catch { }
+                $TeamsDesktopSettingsFile = $TeamsSettingsFile.Directory.FullName + "\desktop-config.json"
+                if (Test-Path $TeamsDesktopSettingsFile -ErrorAction SilentlyContinue) {
+                    $TeamsDesktopSettings = Get-Content -Path $TeamsDesktopSettingsFile
+                    $WindowsUser = ""
+                    $TeamsUserName =""
+                    $RegexTemp = [regex]::Match($TeamsDesktopSettings, $regexWindowsUser).captures.groups
+                    if ($RegexTemp.Count -ge 2) {
+                        $WindowsUser = $RegexTemp[2].value
+                    }
+                    $RegexTemp = [regex]::Match($TeamsDesktopSettings, $regexTeamsUserName).captures.groups
+                    if ($RegexTemp.Count -ge 2) {
+                        $TeamsUserName = $RegexTemp[2].value
+                    }
                 }
-                $EnvTemp = [regex]::Match($TeamsSettings,$regexEnv).captures.groups
-                if($EnvTemp.Count -ge 2){
-                    $Env = $EnvTemp[2].value
+                $TeamsVersion = New-Object -TypeName PSObject -Property @{
+                    WindowsUser             = $WindowsUser
+                    TeamsUser               = $TeamsUserName
+                    Version                 = $Version
+                    Ring                    = $Ring
+                    Environment             = $Env
+                    CloudEnvironment        = $CloudEnv
+                    Region                  = $Region
+                    Path                    = $TeamsSettingsFile.Directory.FullName
                 }
-                $RegionTemp = [regex]::Match($TeamsSettings,$regexRegion).captures.groups
-                if($RegionTemp.Count -ge 2){
-                    $Region = $RegionTemp[2].value
-                }
-            } catch { }
-            $TeamsApp = $Profile.ProfileImagePath + "\AppData\Local\Microsoft\Teams\current\Teams.exe"
-            $InstallDateStr = Get-Content ($Profile.ProfileImagePath + "\AppData\Roaming\Microsoft\Teams\installTime.txt")
-            $TeamsVersion = New-Object –TypeName PSObject -Property @{
-                Profile = (New-Object System.Security.Principal.SecurityIdentifier($Profile.PSChildName)).Translate( [System.Security.Principal.NTAccount]).Value
-                ProfilePath = $Profile.ProfileImagePath
-                Version = $Version
-                Ring = $Ring
-                Environment = $Env
-                Region = $Region
-                Arch = Get-UcArch $TeamsApp
-                InstallDate = [Datetime]::ParseExact($InstallDateStr, 'M/d/yyyy', $null) | Get-Date -Format $currentDateFormat
+                $TeamsVersion.PSObject.TypeNAmes.Insert(0, 'TeamsVersionFromPath')
+                $outTeamsVersion.Add($TeamsVersion) | Out-Null
             }
-            
-            $TeamsVersion.PSObject.TypeNAmes.Insert(0,'TeamsVersion')
-            $outTeamsVersion.Add($TeamsVersion) | Out-Null
+        } else {
+            Write-Error -Message ("Invalid Path, please check if path: " + $path + " is correct and exists.")
+        }
+
+    } else {
+        $currentDateFormat = [cultureinfo]::CurrentCulture.DateTimeFormat.ShortDatePattern
+        if($Computer) {
+            $RemotePath = "\\" + $Computer + "\C$\Users\"
+            if(Test-Path -Path $RemotePath) {
+                $Profiles = Get-ChildItem -Path $RemotePath -ErrorAction SilentlyContinue
+            } else {
+                Write-Error -Message ("Cannot get users on : " + $computer + " check if name is correct and permissions.")
+            }
+        } else {
+            $Profiles = Get-childItem 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | ForEach-Object { Get-ItemProperty $_.pspath } | Where-Object { $_.fullprofile -eq 1 }
+        }
+        
+        foreach ($UserProfile in $Profiles) {
+            if($Computer){
+                $ProfilePath = $UserProfile.FullName
+                $ProfileName = $UserProfile.Name
+            } else {
+                $ProfilePath = $UserProfile.ProfileImagePath
+                $ProfileName  = (New-Object System.Security.Principal.SecurityIdentifier($UserProfile.PSChildName)).Translate( [System.Security.Principal.NTAccount]).Value
+            }
+            $TeamsSettingPath = $ProfilePath + "\AppData\Roaming\Microsoft\Teams\settings.json"
+            if (Test-Path $TeamsSettingPath -ErrorAction SilentlyContinue) {
+                $TeamsSettings = Get-Content -Path $TeamsSettingPath
+                $Version = ""
+                $Ring = ""
+                $Env = ""
+                $CloudEnv = ""
+                $Region = ""
+                try {
+                    $VersionTemp = [regex]::Match($TeamsSettings, $regexVersion).captures.groups
+                    if ($VersionTemp.Count -ge 2) {
+                        $Version = $VersionTemp[2].value
+                    }
+                    $RingTemp = [regex]::Match($TeamsSettings, $regexRing).captures.groups
+                    if ($RingTemp.Count -ge 2) {
+                        $Ring = $RingTemp[2].value
+                    }
+                    $EnvTemp = [regex]::Match($TeamsSettings, $regexEnv).captures.groups
+                    if ($EnvTemp.Count -ge 2) {
+                        $Env = $EnvTemp[2].value
+                    }
+                    $CloudEnvTemp = [regex]::Match($TeamsSettings, $regexCloudEnv).captures.groups
+                    if ($CloudEnvTemp.Count -ge 2) {
+                        $CloudEnv = $CloudEnvTemp[2].value
+                    }
+                    $RegionTemp = [regex]::Match($TeamsSettings, $regexRegion).captures.groups
+                    if ($RegionTemp.Count -ge 2) {
+                        $Region = $RegionTemp[2].value
+                    }
+                }
+                catch { }
+                $TeamsApp = $ProfilePath + "\AppData\Local\Microsoft\Teams\current\Teams.exe"
+                $InstallDateStr = Get-Content ($ProfilePath + "\AppData\Roaming\Microsoft\Teams\installTime.txt")
+                $TeamsVersion = New-Object -TypeName PSObject -Property @{
+                    Profile          = $ProfileName
+                    ProfilePath      = $ProfilePath
+                    Version          = $Version
+                    Ring             = $Ring
+                    Environment      = $Env
+                    CloudEnvironment = $CloudEnv
+                    Region           = $Region
+                    Arch             = Get-UcArch $TeamsApp
+                    InstallDate      = [Datetime]::ParseExact($InstallDateStr, 'M/d/yyyy', $null) | Get-Date -Format $currentDateFormat
+                }
+                $TeamsVersion.PSObject.TypeNAmes.Insert(0, 'TeamsVersion')
+                $outTeamsVersion.Add($TeamsVersion) | Out-Null
+            }
         }
     }
     return $outTeamsVersion
